bcdc80e62f5fded795817ea4e52987502c9b9f96
Trey Mosley <iiimosley@gmail.com>
2018-03-23 13:49:01 -0500

    return appropriate error message for blocking delete

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -28,10 +28,10 @@ module.exports.putProduct = (req, res, next) => {
 }
 
 module.exports.deleteProduct = (req, res, next) => {
-    res.error = "please dont delete anything";
-    console.log(res.error);
-    next(res.error);
-    // deleteOne(req.params.id)
-    //     .then(product => res.status(200).json(product))
-    //     .catch(err => next(err));
+
+    let error = new Error("please dont delete anything");
+    error.status = 405;
+    console.log(error);
+    next(error);
 }
+

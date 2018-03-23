32e6607e30135a7370bad836ff0424990f73d9de
Courtney Brothers <courtneywbrothers@gmail.com>
2018-03-23 15:40:52 -0500

    expose departments resource

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,5 @@ const router = Router();
 // router.use(require('./<FILE NAME>));
 router.use(require('./customerRoute'));
 router.use(require('./employeeRoute'));
+router.use(require('./departmentRoute'));
 module.exports = router;

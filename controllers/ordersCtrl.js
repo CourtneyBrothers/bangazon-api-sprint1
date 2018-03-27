434cad94510ecb9e2ee989fd9b93feaaa989e2dd
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-03-26 19:42:22 -0500

    cleaned up code

--- controllers/ordersCtrl.js
+++ controllers/ordersCtrl.js
@@ -8,104 +8,104 @@ const productModel = require('../models/Product');
 //gets all orders, then querys our join table referencing the order id, then querys products based on the product id received from the join table. The product data is then added to the appropriate Order as a property.
 module.exports.getAllOrders = (req, res, next) => {
     getAll()
-    .then(orders => {
-        let ordersArray = [];
-        orders.forEach(order => {
-            let productArray = [];
-            getProductsInOrder(order.order_id)
-            .then(prods => {
-                prods.forEach(prodId => {
-                    productModel.getOne(prodId)
-                    .then(productInfo => {
-                        productArray.push(productInfo); 
+        .then(orders => {
+            let ordersArray = [];
+            orders.forEach(order => {
+                let productArray = [];
+                getProductsInOrder(order.order_id)
+                    .then(prods => {
+                        prods.forEach(prodId => {
+                            productModel.getOne(prodId)
+                                .then(productInfo => {
+                                    productArray.push(productInfo);
+                                })
+                            order.products = productArray;
+                        })
                     })
-                    order.products = productArray;
-                })
-            })
-            ordersArray.push(order);
-        });
-        setTimeout(function(){
-            res.status(200).json(ordersArray);
-        },
-    100);
-    })
-    .catch(err => next(err));
+                ordersArray.push(order);
+            });
+            setTimeout(function () {
+                res.status(200).json(ordersArray);
+            },
+                100);
+        })
+        .catch(err => next(err));
 };
 
 //same process as above, except for retrieving a single order
-module.exports.getOneOrder = ( { params: { id } }, res, next) => {
+module.exports.getOneOrder = ({ params: { id } }, res, next) => {
     getOne(id)
-    .then(order => {
-        if (order) {
-            let productArray = [];
-            getProductsInOrder(id)
-            .then(prods => {
-                prods.forEach(prodId => {
-                    productModel.getOne(prodId)
-                        .then(productInfo => {
-                            productArray.push(productInfo);
+        .then(order => {
+            if (order) {
+                let productArray = [];
+                getProductsInOrder(id)
+                    .then(prods => {
+                        prods.forEach(prodId => {
+                            productModel.getOne(prodId)
+                                .then(productInfo => {
+                                    productArray.push(productInfo);
+                                });
+                            order.products = productArray;
                         });
-                        order.products = productArray;
+                        setTimeout(() => {
+                            res.status(200).json(order);
+                        }, 100);
                     });
-                    setTimeout(() => {
-                        res.status(200).json(order);
-                    }, 100);
-                });
-        } else {
-            let error = new Error("Order not found")
-            next(error)
-        }
-    })
-    .catch(err => next(err));  
+            } else {
+                let error = new Error("Order not found")
+                next(error)
+            }
+        })
+        .catch(err => next(err));
 };
 
 // POST
 // Checks customer_id and payment_type passed in via JSON to ensure a new, active order may not be added if an active order already exists for the customer
 module.exports.postOneOrder = (req, res, next) => {
     getByCustomer(req.body)
-    .then(orders => {
-        let paymentTypeArray = [];
-        orders.forEach(order => {
-            if (order.payment_type === null) {
-                paymentTypeArray.push(order);
-            }
-        })
-        if (!paymentTypeArray.length || req.body.payment_type !== null ){
-            postOne(req.body)
-            .then(newOrder => {
-                res.status(200).json(newOrder);
+        .then(orders => {
+            let paymentTypeArray = [];
+            orders.forEach(order => {
+                if (order.payment_type === null) {
+                    paymentTypeArray.push(order);
+                }
             })
-        } else {
-            let error = new Error("Customers may only have one active order at a time!");
-            error.status = 405;
-            next(error);
-        }
-    })
-}
+            if (!paymentTypeArray.length || req.body.payment_type !== null) {
+                postOne(req.body)
+                    .then(newOrder => {
+                        res.status(200).json(newOrder);
+                    })
+            } else {
+                let error = new Error("Customers may only have one active order at a time!");
+                error.status = 405;
+                next(error);
+            }
+        });
+};
 
 // PUT
 module.exports.putOneOrder = (req, res, next) => {
     putOne(req.params, req.body)
-    .then(order => {
-        res.status(200).json(order)
-    })
-    .catch(err => next(err));
-}
+        .then(order => {
+            res.status(200).json(order)
+        })
+        .catch(err => next(err));
+};
 
 // DELETE
 module.exports.deleteOneOrder = (req, res, next) => {
     getOne(req.params.id)
-    .then(orders => {
-        if (orders.payment_type === null) {
-            deleteOne(req.params.id)
-            .then(order => {
-                res.status(200).json(order)
-            })
-            .catch(err => next(err));
-        } else {
-            let error = new Error("You cannot delete an order that has been completed")
-            error.status = 405;
-            next(error)
-        }
-    })
-}
+        .then(orders => {
+            if (orders.payment_type === null) {
+                deleteOne(req.params.id)
+                    .then(order => {
+                        res.status(200).json(order)
+                    })
+                    .catch(err => next(err));
+            } else {
+                let error = new Error("You cannot delete an order that has been completed")
+                error.status = 405;
+                next(error)
+            }
+        });
+};
